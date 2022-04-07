6973fa343363ebbc504dd00b83d94e93dd3f832d
fer0o <fer0osennin@gmail.com>
2022-04-06 23:08:10 -0500

    agregando cambios menores

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App () {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+        <img
+          src={'https://miro.medium.com/max/1156/1*NYoJyNtt8k0vykHEYmih9A.png'}
+        />
         <p className='text-3xl font-bold underline'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
