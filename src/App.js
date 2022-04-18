1426416997ed5b74f2307bdc9b0c63612d8c4591
fer0o <fer0osennin@gmail.com>
2022-04-18 06:13:25 -0500

    agregando daisyUI y modificando navbar, ahora se muestra en app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App () {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img
-          src={'https://miro.medium.com/max/1156/1*NYoJyNtt8k0vykHEYmih9A.png'}
-        />
-        <p className='text-3xl font-bold underline'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className='text-red-700'>Agregando tailwind</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
     </div>
+    // <div className='App'>
+    //   <header className='App-header'>
+    //     <img
+    //       src={'https://miro.medium.com/max/1156/1*NYoJyNtt8k0vykHEYmih9A.png'}
+    //     />
+    //     <p className='text-3xl font-bold underline'>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <p className='text-red-700'>Agregando tailwind</p>
+    //     <a
+    //       className='App-link'
+    //       href='https://reactjs.org'
+    //       target='_blank'
+    //       rel='noopener noreferrer'
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   )
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+const Navbar = () => {
+  return (
+    <div className='navbar bg-emerald-700'>
+      <div className='flex-1'>
+        <img
+          className='w-14 h-14  '
+          src={'http://cdn.onlinewebfonts.com/svg/img_571584.png'}
+        />
+        <a className='btn btn-ghost normal-case text-xl'>Mundialito</a>
+      </div>
+      <div className='flex-none'>
+        <ul className='menu menu-horizontal p-0'>
+          <li>
+            <a href='/'>Inicio</a>
+          </li>
+          <li>
+            <a href='/'>Hombre</a>
+          </li>
+          <li>
+            <a href='/'>Mujer</a>
+          </li>
+          <li>
+            <a href='/'>Niños</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
