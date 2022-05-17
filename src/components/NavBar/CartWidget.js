521c2791e2b400dde162843695c4bd7f50508e85
fer0o <fer0osennin@gmail.com>
2022-05-16 19:59:13 -0500

    logica no completa cart

--- src/components/NavBar/CartWidget.js
+++ src/components/NavBar/CartWidget.js
@@ -1,20 +1,32 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useCartContext } from '../context/CartContext'
 const CartWidget = () => {
+  const [cartClick, setCartClick] = useState(false)
+  const { cart } = useCartContext()
+  let itemsCartW = 0
+  for (let i = 0; i < cart.lenght; i++) {
+    const cant = cart[i].quantity
+    itemsCartW = itemsCartW + cant
+  }
   return (
     <div>
-      <label className='btn btn-ghost btn-circle'>
-        <div className='indicator'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-5 w-5'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-          >
-            <path d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z' />
-          </svg>
-          <span className='badge badge-sm indicator-item'>0</span>
-        </div>
-      </label>
+      <div className='cartWidget' onClick={e => setCartClick(!cartClick)}>
+        <span className='cartCant'>{itemsCartWidg}</span>
+        <i className='fas fa-shopping-cart cart-style'>
+          <p></p>
+        </i>
+
+        {cartClick ? (
+          <div className='card-click'>
+            <Link to='/cart' className='btn bg-principal text-white'>
+              Ver Carrito
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   )
 }
